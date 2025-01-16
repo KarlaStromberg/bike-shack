41d08738a696fcd7282419baf37b5550381e2c13
karlaStromberg <karlastromberg@gmail.com>
2025-01-16 13:26:27 -0300

    Add and hide contact form fields

--- assets/contact-form-custom.js
+++ assets/contact-form-custom.js
@@ -1,1 +1,29 @@
 //logic for our custom contact form
+const businessCheckbox = document.getElementById('businessAccount');
+const companyInfoDiv = document.getElementById('companyInfo');
+const contactReason = document.getElementById('contactReason');
+const productNameDiv = document.getElementById('productNameField');
+const orderNumberDiv = document.getElementById('orderNumberField');
+
+businessCheckbox.addEventListener('change', (e) => {
+    if (businessCheckbox.checked) {
+        companyInfoDiv.classList.remove('hidden');
+    } else {
+        companyInfoDiv.classList.add('hidden');
+    }
+
+});
+
+contactReason.addEventListener('change', (e) => {
+
+    productNameDiv.classList.add('hidden');
+    orderNumberDiv.classList.add('hidden');
+
+    if (contactReason.value === 'product') {
+        productNameDiv.classList.remove('hidden');
+
+    } else if (contactReason.value === 'order') {
+        orderNumberDiv.classList.remove('hidden');
+    }
+
+});
