c2cbca798ee90d185193e5f1f011012b731c6dd1
karlaStromberg <karlastromberg@gmail.com>
2025-05-06 13:32:48 -0300

    Adding logic for the countdown timer

--- assets/countdown.js
+++ assets/countdown.js
@@ -3,17 +3,49 @@ class CountDownTimer extends HTMLElement {
     super();
 
     //Grab required elements
+    this.countDownText = this.querySelector('.countdown-timer');
+
+    this.daysContainer = this.querySelector(".days");
+    this.hoursContainer = this.querySelector(".hours");
+    this.minutesContainer = this.querySelector(".minutes");
+    this.secondsContainer = this.querySelector(".seconds");
 
     // Set Date
-    this.endDateString = "May 25, 2026 16:37:52 EST";
+    this.endDateString = this.countDownText.dataset.enddate;
     this.endDate = new Date(this.endDateString).getTime();
 
+
     // Start timer
+    this.interval = setInterval(this.handleTick.bind(this), 1000)
   }
 
-  handleTick() {
-    //logic and update elements
+    handleTick() {
+      //logic and update elements
+      let now = new Date().getTime();
+      let timeleft = this.endDate - now;
+        
+      if (timeleft < 0) {
+      this.countDownText.innerHTML = "This sale ended!";
+      console.log("stop ticking");
+      clearInterval(this.interval);
+      return;
+    }
+
+    let msInDay = 1000 * 60 * 60 * 24;
+    let msInHour = 1000 * 60 * 60;
+    let msInMinute = 1000 * 60;
+
+    let days = Math.floor(timeleft / msInDay);
+    let hours = Math.floor((timeleft % msInDay) / msInHour);
+    let minutes = Math.floor((timeleft % msInHour) / msInMinute);
+    let seconds = Math.floor((timeleft % msInMinute) / 1000);
+
+    this.daysContainer.innerHTML = days + "d ";
+    this.hoursContainer.innerHTML = hours + "h ";
+    this.minutesContainer.innerHTML = minutes + "m ";
+    this.secondsContainer.innerHTML = seconds + "s";
   }
+
 }
 
 customElements.define("countdown-timer", CountDownTimer);
